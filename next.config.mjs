ba033c257dea3e605e404f4549e9ccd777dcd238
Nadim Salah <salaheddinenadim@gmail.com>
2026-07-05 10:20:01 +0100

    fix: add Cache-Control headers in next.config.mjs to prevent HTML page caching and solve unstyled HTML renders

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,28 @@ const nextConfig = {
       { protocol: "https", hostname: "images.unsplash.com" },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "no-store, no-cache, must-revalidate, proxy-revalidate",
+          },
+        ],
+      },
+      {
+        source: "/_next/static/:path*",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
